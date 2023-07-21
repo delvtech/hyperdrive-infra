8250c2c640494336dcc22cddf4d2bcd3bbdad7b6
Sheng Lundquist <sheng@delv.tech>
2023-07-20 18:22:48 -0700

    Adding username reg server to docker

--- setup_env.sh
+++ setup_env.sh
@@ -8,42 +8,49 @@ if [[ $# -eq 0 ]] || [[ "$1" == "--help" ]]; then
     echo "Usage: ./script_name.sh [flags]"
     echo "Flags:"
     echo "  --devnet    : Spin up an Anvil node, deploy Hyperdrive to it, and serve artifacts on an nginx server."
-    echo "  --bots      : Runs the bot framework, receiving bot configs from a web interface."
+    echo "  --data      : Runs the data framework, querying the chain and writing to postgres."
     echo "  --frontend  : Build the frontend container."
     echo "  --ports     : Expose docker images to your machine, as specified in env/env.ports."
-    echo "  --all       : Enable all components: devnet, bots, frontend, and ports."
-    echo "  --develop   : Enable devnet, bots and ports. Suitable for local development work."
+    echo "  --all       : Enable all components: devnet, data, frontend, and ports."
+    echo "  --develop   : Enable devnet, data, postgres, and ports. Suitable for local development work."
+    echo "  --ec2       : Enable devnet, data, and ports. Need configuration to external postgres."
     exit 0
 fi
 
 # Parse all of the arguments
 ## Initialize variables
 DEVNET=false
-BOTS=false
 PORTS=false
 FRONTEND=false
 POSTGRES=false
+DATA=false
 
 ## Loop through the arguments
 while [[ $# -gt 0 ]]; do
     case $1 in
         --all)
             DEVNET=true
-            BOTS=true
             FRONTEND=true
             POSTGRES=true
             PORTS=true
+            DATA=true
             ;;
         --develop)
             DEVNET=true
             POSTGRES=true
             PORTS=true
+            DATA=true
+            ;;
+        --ec2)
+            DEVNET=true
+            PORTS=true
+            DATA=true
             ;;
         --devnet)
             DEVNET=true
             ;;
-        --bots)
-            BOTS=true
+        --data)
+            DATA=true
             ;;
         --postgres)
             POSTGRES=true
@@ -71,11 +78,11 @@ echo "# Environment for Docker compose" >> .env
 # all of the services in the compose context. We determine which layers are
 # turned on using docker compose profiles.
 devnet_compose="docker-compose.devnet.yaml"
-bot_compose="docker-compose.bots.yaml"
+data_compose="docker-compose.data.yaml"
 frontend_compose="docker-compose.frontend.yaml"
 postgres_compose="docker-compose.postgres.yaml"
 ports_compose="docker-compose.ports.yaml"
-full_compose_files="COMPOSE_FILE=$devnet_compose:$bot_compose:$frontend_compose:$postgres_compose:"
+full_compose_files="COMPOSE_FILE=$devnet_compose:$data_compose:$frontend_compose:$postgres_compose:"
 if $PORTS; then
     full_compose_files+="$ports_compose:"
 fi
@@ -92,17 +99,17 @@ echo $full_compose_files >> .env
 # Set up the COMPOSE_PROFILES environment variable. This toggles which layers
 # should be started.
 frontend_profile="frontend"
-bots_profile="bots"
 postgres_profile="postgres"
+data_profile="data"
 full_compose_profiles="COMPOSE_PROFILES="
 if $FRONTEND; then
     full_compose_profiles+="$frontend_profile,"
 fi
-if $BOTS; then
-    full_compose_profiles+="$bots_profile,"
-fi
 if $POSTGRES; then
     full_compose_profiles+="$postgres_profile,"
+fi
+if $DATA; then
+    full_compose_profiles+="$data_profile,"
 fi
 # Check if "," is at the end of the string
 if [[ $full_compose_profiles == *"," ]]; then
@@ -128,8 +135,9 @@ if $FRONTEND; then
     cat env/env.frontend >> .env
 fi
 
-# optionally add an env.frontend to .env file if --frontend
-if $POSTGRES; then
+# optionally add an env.frontend to .env file if --postgres or --data
+# POSTGRES uses these flags to launch postgres, DATA uses these flags to connect
+if $POSTGRES || $DATA; then
     cat env/env.postgres >> .env
 fi
 
