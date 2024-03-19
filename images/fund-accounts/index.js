d781c654d1630b78f5d934d2949b68e88ef08301
Ryan Goree <goree.ryan@gmail.com>
2024-03-19 06:10:55 -0500

    wip

--- images/fund-accounts/index.js
+++ images/fund-accounts/index.js
@@ -1,8 +1,9 @@
 import { createWalletClient, http, parseUnits } from "viem";
 import { privateKeyToAccount } from "viem/accounts";
 import { foundry } from "viem/chains";
-import accounts from "/accounts/balances.json" assert { type: "json" };
-import addresses from "/artifacts/addresses.json" assert { type: "json" };
+import accounts from "./accounts/balances.json" assert { type: "json" };
+import addresses from "./artifacts/addresses.json" assert { type: "json" };
+import "dotenv/config";
 
 const walletKey = process.env.ADMIN_PRIVATE_KEY;
 const chainId = process.env.CHAIN_ID;
@@ -12,12 +13,19 @@ const account = privateKeyToAccount(walletKey);
 const chain = {
   ...foundry,
   id: Number(chainId),
+  rpcUrls: {
+    default: {
+      http: [rpcUrl],
+      webSocket: [rpcUrl],
+    },
+  },
 };
 const transport = http(rpcUrl);
 
 console.log("account", account.address);
 console.log("chainId", chainId);
 console.log("rpcUrl", rpcUrl);
+console.log("addresses", addresses);
 
 const abi = [
   {
@@ -70,7 +78,7 @@ async function fundAccount(address, eth = 0, tokens = 0) {
 }
 
 // Fund the admin account
-await fundAccount(account.address, 1_000);
+// await fundAccount(account.address, 1_000);
 
 // Fund the accounts in the balances.json file
 for (const [address, { eth, tokens }] of Object.entries(accounts)) {
