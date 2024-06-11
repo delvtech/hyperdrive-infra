fc75736b06a83a1bf512b2eed528e57de44a2cc1
Sheng Lundquist <sheng@delv.tech>
2024-06-11 11:03:21 -0700

    Cleanup and update for agent0

--- setup_env.sh
+++ setup_env.sh
@@ -6,21 +6,20 @@
 # Check if no arguments or --help is passed
 if [[ $# -eq 0 ]] || [[ "$1" == "--help" ]]; then
     echo "Usage: ./setup_env.sh [flags]"
-    echo "Flags:"
+    echo "Flag Groups:"
     echo "  --all            : Fund accounts and enable all components: anvil, data, postgres, frontend, and ports."
-    echo "  --competition    : Fund accounts and enable anvil on block time with testnet image, data (without postgres), and ports. Use this for a trading competition deployment."
     echo "  --develop        : Fund accounts and enable anvil, data, and ports. Suitable for local development work."
-    echo "  --fuzz           : Enable anvil on block time, data, and ports. Runs fuzzbots and outputs crash reports."
+    echo "Flags:"
     echo "  --anvil          : Spin up an Anvil node, deploy Hyperdrive to it, and serve artifacts on an nginx server."
     echo "  --blocktime      : Sets the anvil node to run in blocktime mode."
     echo "  --testnet        : Uses the testnet hyperdrive image with restricted mint access."
     echo "  --frontend       : Build the frontend container."
-    echo "  --data           : Runs the data framework, querying the chain and writing to postgres."
     echo "  --postgres       : Runs a postgres db container for storing data."
+    echo "  --data           : Runs the data framework, querying the chain and writing to postgres."
+    echo "  --service-bots   : Runs checkpoint bot and invariance check bots on the chain."
     echo "  --ports          : Expose docker images to your machine, as specified in env/env.ports."
     echo "  --fund-accounts  : Fund accounts from /accounts/balances.json."
-    echo "  --dynamic-rate   : Yield source will have a dynamic variable rate."
-    echo "  --fuzzbot        : Runs fuzzbots on the chain."
+    echo "  --rate-bot       : Yield source will have a dynamic variable rate."
     exit 0
 fi
 
@@ -30,49 +29,37 @@ ANVIL=false
 BLOCKTIME=false
 TESTNET=false
 FRONTEND=false
-DATA=false
 POSTGRES=false
+DATA=false
 PORTS=false
+SERVICE_BOTS=false
+RATE_BOT=false
 FUND_ACCOUNTS=false
-DYNAMIC_RATE=false
-FUZZBOT=false
 
 ## Loop through the arguments
 while [[ $# -gt 0 ]]; do
     case $1 in
+        # Flag groups
         --all)
             ANVIL=true
+            # BLOCKTIME=true
+            # TODO add in testnet image here
             FRONTEND=true
-            DATA=true
             POSTGRES=true
-            PORTS=true
-            FUND_ACCOUNTS=true
-            DYNAMIC_RATE=true
-            ;;
-        --competition)
-            ANVIL=true
-            BLOCKTIME=true
-            TESTNET=true
             DATA=true
             PORTS=true
+            SERVICE_BOTS=true
+            RATE_BOT=true
             FUND_ACCOUNTS=true
-            DYNAMIC_RATE=true
             ;;
         --develop)
             ANVIL=true
             POSTGRES=true
-            PORTS=true
             DATA=true
+            PORTS=true
             FUND_ACCOUNTS=true
             ;;
-        --fuzz)
-            ANVIL=true
-            BLOCKTIME=true
-            DATA=true
-            POSTGRES=true
-            PORTS=true
-            FUZZBOT=true
-            ;;
+        # Flags
         --anvil)
             ANVIL=true
             ;;
@@ -94,14 +81,14 @@ while [[ $# -gt 0 ]]; do
         --ports)
             PORTS=true
             ;;
+        --service-bots)
+            SERVICE_BOTS=true
+            ;;
+        --rate-bot)
+            RATE_BOT=true
+            ;;
         --fund-accounts)
             FUND_ACCOUNTS=true
-            ;;
-        --dynamic-rate)
-            DYNAMIC_RATE=true
-            ;;
-        --fuzzbot)
-            FUZZBOT=true
             ;;
         *)
             echo "Unknown flag: $1"
@@ -120,15 +107,17 @@ echo "# Environment for Docker compose" >>.env
 # all of the services in the compose context. We determine which layers are
 # turned on using docker compose profiles.
 anvil_compose="docker-compose.anvil.yaml"
-data_compose="docker-compose.data.yaml"
-rate_bot_compose="docker-compose.rate-bot.yaml"
-fuzz_bot_compose="docker-compose.fuzz-bot.yaml"
+blocktime_compose="docker-compose.blocktime.yaml"
+testnet_compose="docker-compose.testnet.yaml"
 frontend_compose="docker-compose.frontend.yaml"
 postgres_compose="docker-compose.postgres.yaml"
-testnet_compose="docker-compose.testnet.yaml"
-blocktime_compose="docker-compose.blocktime.yaml"
+data_compose="docker-compose.data.yaml"
+service_bots_compose="docker-compose.service-bots.yaml"
 ports_compose="docker-compose.ports.yaml"
-full_compose_files="COMPOSE_FILE=$anvil_compose:$data_compose:$frontend_compose:$postgres_compose:$rate_bot_compose:$fuzz_bot_compose:"
+
+# We default to using profiles to control which services are turned on.
+full_compose_files="COMPOSE_FILE=$anvil_compose:$frontend_compose:$postgres_compose:$data_compose:$service_bots_compose:"
+# We only add controls here if these compose files update existing services.
 if $BLOCKTIME; then
     full_compose_files+="$blocktime_compose:"
 fi
@@ -150,14 +139,17 @@ echo $full_compose_files >>.env
 
 # Set up the COMPOSE_PROFILES environment variable. This toggles which layers
 # should be started.
+blocktime_profile="blocktime"
 frontend_profile="frontend"
 postgres_profile="postgres"
 data_profile="data"
+service_bots_profile="service-bots"
+rate_bot_profile="rate-bot"
 fund_accounts_profile="fund-accounts"
-blocktime_profile="blocktime"
-dynamic_rate_profile="dynamic-rate"
-fuzz_bot_profile="fuzz"
 full_compose_profiles="COMPOSE_PROFILES="
+if $BLOCKTIME; then
+    full_compose_profiles+="$blocktime_profile,"
+fi
 if $FRONTEND; then
     full_compose_profiles+="$frontend_profile,"
 fi
@@ -167,17 +159,14 @@ fi
 if $DATA; then
     full_compose_profiles+="$data_profile,"
 fi
+if $SERVICE_BOTS; then
+    full_compose_profiles+="$service_bots_profile,"
+fi
+if $RATE_BOT; then
+    full_compose_profiles+="$rate_bot_profile,"
+fi
 if $FUND_ACCOUNTS; then
     full_compose_profiles+="$fund_accounts_profile,"
-fi
-if $BLOCKTIME; then
-    full_compose_profiles+="$blocktime_profile,"
-fi
-if $DYNAMIC_RATE; then
-    full_compose_profiles+="$dynamic_rate_profile,"
-fi
-if $FUZZBOT; then
-    full_compose_profiles+="$fuzz_bot_profile,"
 fi
 # Check if "," is at the end of the string
 if [[ $full_compose_profiles == *"," ]]; then
@@ -206,6 +195,12 @@ if $ANVIL; then
     cat env/env.anvil >>.env
 fi
 
+# optionally add an env.time to .env file if --blocktime
+if $BLOCKTIME; then
+    echo "" >>.env
+    cat env/env.time >>.env
+fi
+
 # optionally add an env.frontend to .env file if --frontend
 if $FRONTEND; then
     echo "" >>.env
@@ -213,16 +208,11 @@ if $FRONTEND; then
 fi
 
 # optionally add an env.frontend to .env file if --postgres or --data
-# POSTGRES uses these flags to launch postgres, DATA uses these flags to connect
-if $POSTGRES || $DATA; then
+# POSTGRES uses these flags to launch postgres. 
+# All agent0 images uses these flags to connect
+if $POSTGRES || $DATA || $SERVICE_BOTS; then
     echo "" >>.env
     cat env/env.postgres >> .env
-fi
-
-# optionally add an env.time to .env file if --blocktime
-if $BLOCKTIME; then
-    echo "" >>.env
-    cat env/env.time >>.env
 fi
 
 echo "Environment filed created at .env"
